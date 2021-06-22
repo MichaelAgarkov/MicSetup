0cdba228385ee2076a2e772444e14a2c98a29702
Michael Agarkov <michaelagarkov@outlook.com>
2021-06-22 16:28:37 +0300

    Add files via upload
    So apparently after almost a month, I realized I forgot to update the source code with the new version of MicSetup... What is actually wrong with me

--- Unit1.pas
+++ Unit1.pas
@@ -18,6 +18,9 @@ type
     procedure showInstallersToolStripMenuItem_Click(sender: Object; e: EventArgs);
     procedure closeMicSetupToolStripMenuItem_Click(sender: Object; e: EventArgs);
     procedure checkBox1_CheckedChanged(sender: Object; e: EventArgs);
+    procedure button4_Click(sender: Object; e: EventArgs);
+    procedure richTextBox1_TextChanged(sender: Object; e: EventArgs);
+    procedure saveFileDialog1_FileOk(sender: Object; e: CancelEventArgs);
   {$region FormDesigner}
   private
     {$resource Unit1.Form1.resources}
@@ -50,6 +53,9 @@ type
     textBox4: TextBox;
     label5: &Label;
     checkBox1: CheckBox;
+    button4: Button;
+    richTextBox1: RichTextBox;
+    saveFileDialog1: SaveFileDialog;
     treeView1: TreeView;
     {$include Unit1.Form1.inc}
   {$endregion FormDesigner}
@@ -110,19 +116,25 @@ procedure Form1.button3_Click(sender: Object; e: EventArgs);
 begin
   if((textBox1.Text <> '') and (textBox2.Text <> '') and ((textBox3.Text = '') and checkBox1.Checked = false) and (textBox4.Text <> '')) then begin
     label5.Visible := false;
+    richTextBox1.Text := 'MicSetup v2.0' + NewLine + '======== Build log ========' + NewLine;
     System.IO.Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + '\MicSetup Installers\' + textBox1.Text + '\' + textBox4.Text);
+    richTextBox1.Text := richTextBox1.Text + 'Output folder: ' + Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + '\MicSetup Installers\' + textBox1.Text + '\' + textBox4.Text + NewLine;
     progressBar1.Value := 1;
     WriteLines(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + '\MicSetup Installers\' + textBox1.Text + '\' + textBox4.Text + '\' + 'programname.cfg', textBox1.Lines);
     progressBar1.Value := 10;
     WriteLines(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + '\MicSetup Installers\' + textBox1.Text + '\' + textBox4.Text + '\' + 'programver.cfg', textBox4.Lines);
+    richTextBox1.Text := richTextBox1.Text + 'Created config files' + NewLine;
     if(checkBox1.Checked) then begin
       progressBar1.Value := 20;
       System.IO.File.Copy(textBox3.Text, Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + '\MicSetup Installers\' + textBox1.Text + '\' + textBox4.Text + '\' + 'License.txt');
+      richTextBox1.Text := richTextBox1.Text + 'Added EULA: License.txt' + NewLine;
     end;
     progressBar1.Value := 30;
     System.IO.File.Copy(textBox2.Text, Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + '\MicSetup Installers\' + textBox1.Text + '\' + textBox4.Text + '\' + textBox1.Text + '.exe');
+    richTextBox1.Text := richTextBox1.Text + 'Copied main executable: ' + textBox1.Text + '.exe' + NewLine;
     progressBar1.Value := 50;
     System.IO.File.Copy('Installer.engine', Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + '\MicSetup Installers\' + textBox1.Text + '\' + textBox4.Text + '\' + textBox1.Text + 'Installer.exe');
+    richTextBox1.Text := richTextBox1.Text + 'Created installer: ' + textBox1.Text + 'Installer.exe' + NewLine + 'Installation building process finished';
     progressBar1.Value := 100;
   end else label5.Visible := true;
 end;
@@ -132,19 +144,25 @@ begin
   treeView1.SelectedNode := treeView1.Nodes[2];
   if((textBox1.Text <> '') and (textBox2.Text <> '') and ((textBox3.Text = '') and checkBox1.Checked = false) and (textBox4.Text <> '')) then begin
     label5.Visible := false;
+    richTextBox1.Text := 'MicSetup v2.0' + NewLine + '======== Build log ========' + NewLine;
     System.IO.Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + '\MicSetup Installers\' + textBox1.Text + '\' + textBox4.Text);
+    richTextBox1.Text := richTextBox1.Text + 'Output folder: ' + Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + '\MicSetup Installers\' + textBox1.Text + '\' + textBox4.Text + NewLine;
     progressBar1.Value := 1;
     WriteLines(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + '\MicSetup Installers\' + textBox1.Text + '\' + textBox4.Text + '\' + 'programname.cfg', textBox1.Lines);
     progressBar1.Value := 10;
     WriteLines(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + '\MicSetup Installers\' + textBox1.Text + '\' + textBox4.Text + '\' + 'programver.cfg', textBox4.Lines);
+    richTextBox1.Text := richTextBox1.Text + 'Created config files' + NewLine;
     if(checkBox1.Checked) then begin
       progressBar1.Value := 20;
       System.IO.File.Copy(textBox3.Text, Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + '\MicSetup Installers\' + textBox1.Text + '\' + textBox4.Text + '\' + 'License.txt');
+      richTextBox1.Text := richTextBox1.Text + 'Added EULA: License.txt' + NewLine;
     end;
     progressBar1.Value := 30;
     System.IO.File.Copy(textBox2.Text, Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + '\MicSetup Installers\' + textBox1.Text + '\' + textBox4.Text + '\' + textBox1.Text + '.exe');
+    richTextBox1.Text := richTextBox1.Text + 'Copied main executable: ' + textBox1.Text + '.exe' + NewLine;
     progressBar1.Value := 50;
     System.IO.File.Copy('Installer.engine', Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + '\MicSetup Installers\' + textBox1.Text + '\' + textBox4.Text + '\' + textBox1.Text + 'Installer.exe');
+    richTextBox1.Text := richTextBox1.Text + 'Created installer: ' + textBox1.Text + 'Installer.exe' + NewLine + 'Installation building process finished';
     progressBar1.Value := 100;
   end else label5.Visible := true;
 end;
@@ -169,6 +187,24 @@ begin
   textBox3.Enabled := checkBox1.Checked;
   button2.Enabled := checkBox1.Checked;
   label4.Enabled := checkBox1.Checked;
+end;
+
+procedure Form1.button4_Click(sender: Object; e: EventArgs);
+begin
+  saveFileDialog1.FileName := textBox1.Text + ' ' + textBox4.Text + ' setup build';
+  saveFileDialog1.ShowDialog;
+end;
+
+procedure Form1.richTextBox1_TextChanged(sender: Object; e: EventArgs);
+begin
+  button4.Enabled := true;
+end;
+
+procedure Form1.saveFileDialog1_FileOk(sender: Object; e: CancelEventArgs);
+begin
+  var Log := new System.IO.StreamWriter(saveFileDialog1.FileName, false, System.Text.Encoding.Default);
+  Log.Write(richTextBox1.Text);
+  Log.Close;
 end;
 
 end.
